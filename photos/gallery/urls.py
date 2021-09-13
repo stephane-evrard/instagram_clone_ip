e40cd35c735bd789b600148e81c8a508e9a9f647
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-13 22:03:18 +0300

    urls

---
+++ photos/gallery/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    url(r'^$', views.index, name= 'index'),
+    url(r'^search/', views.search_images, name='search_results'),
+    url(r'^image/(\d+)', views.get_image, name='image_results'),
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    url(r'^like/(\d+)', views.like_image, name='like_image'),
+    url(r'^logout/$', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout')
+    # url(r'^logout/', views.logout_view, name='logout'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
